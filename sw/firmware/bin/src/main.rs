781941f0c37602bd8a5f8e4accac684b55d6312d
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2019-03-23 17:56:21 +0100

    fw: move configure code from main to system.setup.

--- sw/firmware/bin/src/main.rs
+++ sw/firmware/bin/src/main.rs
@@ -30,8 +30,6 @@ fn main() -> ! {
     let device_peripherals = DevicePeripherals::take().expect("Can not take device peripherals");
 
     free(|cs| {
-        configure(&device_peripherals, &mut core_peripherals);
-
         let mut system = system::System::new(
             device_peripherals,
             systick::create(core_peripherals.SYST),
@@ -41,6 +39,18 @@ fn main() -> ! {
         system.setup();
 
         *SYSTEM.borrow(cs).borrow_mut() = Some(system);
+
+        // Configure interrupts and enable.
+        unsafe {
+            core_peripherals.NVIC.set_priority(Interrupt::EXTI0_1, 1);
+            core_peripherals.NVIC.set_priority(Interrupt::EXTI2_3, 1);
+            core_peripherals.NVIC.set_priority(Interrupt::RTC, 2);
+        }
+
+        core_peripherals.NVIC.enable(Interrupt::EXTI0_1);
+        core_peripherals.NVIC.enable(Interrupt::EXTI2_3);
+        core_peripherals.NVIC.enable(Interrupt::RTC);
+        core_peripherals.NVIC.enable(Interrupt::USB);
     });
 
     loop {
@@ -89,124 +99,4 @@ where
             panic!("Can not borrow application state!");
         }
     });
-}
-
-/// Initialize the system, configure clock, GPIOs and interrupts.
-fn configure(device: &DevicePeripherals, core: &mut CorePeripherals) {
-    // Remap PA9-10 to PA11-12 for USB.
-    device.RCC.apb2enr.modify(|_, w| w.syscfgen().set_bit());
-    device
-        .SYSCFG
-        .cfgr1
-        .modify(|_, w| w.pa11_pa12_rmp().set_bit().mem_mode().main_flash());
-
-    // -----------Buttons----------------
-
-    // Enable EXTI0 interrupt line for PA0 and EXTI2 for PA2.
-    device
-        .SYSCFG
-        .exticr1
-        .modify(|_, w| w.exti0().pa0().exti2().pa2());
-
-    // Configure PA0/PA2 to trigger an interrupt event on the EXTI0/EXTI2 line on a rising edge.
-    device
-        .EXTI
-        .rtsr
-        .modify(|_, w| w.tr0().set_bit().tr2().set_bit());
-
-    // Unmask the external interrupt line EXTI0\EXTI2 by setting the bit corresponding to the
-    // EXTI0\EXTI2 "bit 0/2" in the EXT_IMR register.
-    device
-        .EXTI
-        .imr
-        .modify(|_, w| w.mr0().set_bit().mr2().set_bit());
-
-    // ---------GPIO------------------
-
-    // Enable clock for GPIO Port A, B and F.
-    device
-        .RCC
-        .ahbenr
-        .modify(|_, w| w.iopaen().set_bit().iopben().set_bit().iopfen().set_bit());
-
-    // Switch PA0 (button), PA2 (button), PA7 (beeper), PA11 and PA12 (usb) to alternate function
-    // mode and PA1, PA3-6 to AIN to reduce power consumption.
-    device.GPIOA.moder.modify(|_, w| {
-        w.moder0()
-            .alternate()
-            .moder1()
-            .analog()
-            .moder2()
-            .alternate()
-            .moder3()
-            .analog()
-            .moder4()
-            .analog()
-            .moder5()
-            .analog()
-            .moder6()
-            .analog()
-            .moder7()
-            .alternate()
-            .moder11()
-            .alternate()
-            .moder12()
-            .alternate()
-    });
-
-    // Enable AIN for GPIO B and F to reduce power consumption.
-    device
-        .GPIOB
-        .moder
-        .modify(|_, w| w.moder1().analog().moder8().analog());
-    device
-        .GPIOF
-        .moder
-        .modify(|_, w| w.moder0().analog().moder1().analog());
-
-    device
-        .RCC
-        .ahbenr
-        .modify(|_, w| w.iopben().clear_bit().iopfen().clear_bit());
-
-    // Enable pull-down for PA0 and PA2.
-    device
-        .GPIOA
-        .pupdr
-        .modify(|_, w| w.pupdr0().pull_down().pupdr2().pull_down());
-
-    // Set "high" output speed for PA7, PA11 and PA12.
-    device.GPIOA.ospeedr.modify(|_, w| {
-        w.ospeedr7()
-            .very_high_speed()
-            .ospeedr11()
-            .very_high_speed()
-            .ospeedr12()
-            .very_high_speed()
-    });
-
-    // Set alternative function #2 for PA0 (WKUP1), PA2 (WKUP4) and PA7 (TIM1_CH1N).
-    device
-        .GPIOA
-        .afrl
-        .modify(|_, w| w.afrl0().af2().afrl2().af2().afrl7().af2());
-
-    // Set alternative function #2 (USB) for PA11 and PA12.
-    device
-        .GPIOA
-        .afrh
-        .modify(|_, w| w.afrh11().af2().afrh12().af2());
-
-    // Set priority for the interrupts
-    unsafe {
-        core.NVIC.set_priority(Interrupt::EXTI0_1, 1);
-        core.NVIC.set_priority(Interrupt::EXTI2_3, 1);
-        core.NVIC.set_priority(Interrupt::RTC, 2);
-    }
-
-    // Enable the interrupt in the NVIC.
-    core.NVIC.enable(Interrupt::EXTI0_1);
-    core.NVIC.enable(Interrupt::EXTI2_3);
-    core.NVIC.enable(Interrupt::RTC);
-    core.NVIC.enable(Interrupt::USB);
 }
--- sw/firmware/bin/src/system.rs
+++ sw/firmware/bin/src/system.rs
@@ -54,6 +54,110 @@ impl<S: SysTickHardware> System<S> {
     }
 
     pub fn setup(&mut self) {
+        // Remap PA9-10 to PA11-12 for USB.
+        self.p.RCC.apb2enr.modify(|_, w| w.syscfgen().set_bit());
+        self.p
+            .SYSCFG
+            .cfgr1
+            .modify(|_, w| w.pa11_pa12_rmp().set_bit().mem_mode().main_flash());
+
+        // -----------Buttons----------------
+
+        // Enable EXTI0 interrupt line for PA0 and EXTI2 for PA2.
+        self.p
+            .SYSCFG
+            .exticr1
+            .modify(|_, w| w.exti0().pa0().exti2().pa2());
+
+        // Configure PA0/PA2 to trigger an interrupt event on the EXTI0/EXTI2 line on a rising edge.
+        self.p
+            .EXTI
+            .rtsr
+            .modify(|_, w| w.tr0().set_bit().tr2().set_bit());
+
+        // Unmask the external interrupt line EXTI0\EXTI2 by setting the bit corresponding to the
+        // EXTI0\EXTI2 "bit 0/2" in the EXT_IMR register.
+        self.p
+            .EXTI
+            .imr
+            .modify(|_, w| w.mr0().set_bit().mr2().set_bit());
+
+        // ---------GPIO------------------
+
+        // Enable clock for GPIO Port A, B and F.
+        self.p
+            .RCC
+            .ahbenr
+            .modify(|_, w| w.iopaen().set_bit().iopben().set_bit().iopfen().set_bit());
+
+        // Switch PA0 (button), PA2 (button), PA7 (beeper), PA11 and PA12 (usb) to alternate function
+        // mode and PA1, PA3-6 to AIN to reduce power consumption.
+        self.p.GPIOA.moder.modify(|_, w| {
+            w.moder0()
+                .alternate()
+                .moder1()
+                .analog()
+                .moder2()
+                .alternate()
+                .moder3()
+                .analog()
+                .moder4()
+                .analog()
+                .moder5()
+                .analog()
+                .moder6()
+                .analog()
+                .moder7()
+                .alternate()
+                .moder11()
+                .alternate()
+                .moder12()
+                .alternate()
+        });
+
+        // Enable AIN for GPIO B and F to reduce power consumption.
+        self.p
+            .GPIOB
+            .moder
+            .modify(|_, w| w.moder1().analog().moder8().analog());
+        self.p
+            .GPIOF
+            .moder
+            .modify(|_, w| w.moder0().analog().moder1().analog());
+
+        self.p
+            .RCC
+            .ahbenr
+            .modify(|_, w| w.iopben().clear_bit().iopfen().clear_bit());
+
+        // Enable pull-down for PA0 and PA2.
+        self.p
+            .GPIOA
+            .pupdr
+            .modify(|_, w| w.pupdr0().pull_down().pupdr2().pull_down());
+
+        // Set "high" output speed for PA7, PA11 and PA12.
+        self.p.GPIOA.ospeedr.modify(|_, w| {
+            w.ospeedr7()
+                .very_high_speed()
+                .ospeedr11()
+                .very_high_speed()
+                .ospeedr12()
+                .very_high_speed()
+        });
+
+        // Set alternative function #2 for PA0 (WKUP1), PA2 (WKUP4) and PA7 (TIM1_CH1N).
+        self.p
+            .GPIOA
+            .afrl
+            .modify(|_, w| w.afrl0().af2().afrl2().af2().afrl7().af2());
+
+        // Set alternative function #2 (USB) for PA11 and PA12.
+        self.p
+            .GPIOA
+            .afrh
+            .modify(|_, w| w.afrh11().af2().afrh12().af2());
+
         self.buttons().setup();
 
         self.set_mode(SystemMode::Idle);
