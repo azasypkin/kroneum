bc4a857fdd835cb3504ab44ed40c7925d3998715
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2019-03-24 13:00:14 +0100

    fw: implement `usb::CommandPacket.to_bytes`.

--- sw/firmware/api/src/usb/command_packet.rs
+++ sw/firmware/api/src/usb/command_packet.rs
@@ -1,5 +1,8 @@
 use crate::time::Time;
 
+const COMMAND_BYTE_SEQUENCE_LENGTH: usize = 6;
+pub type CommandByteSequence = [u8; COMMAND_BYTE_SEQUENCE_LENGTH];
+
 #[derive(Copy, Clone, Debug, PartialOrd, PartialEq)]
 pub enum CommandPacket {
     Unknown,
@@ -9,18 +12,30 @@ pub enum CommandPacket {
     SetAlarm(Time),
 }
 
+impl CommandPacket {
+    pub fn to_bytes(self) -> CommandByteSequence {
+        match self {
+            CommandPacket::Beep(n_beeps) => [1, 0, n_beeps, 0, 0, 0],
+            CommandPacket::SetAlarm(time) => [2, 0, time.hours, time.minutes, time.seconds, 0],
+            CommandPacket::GetAlarm => [3, 0, 0, 0, 0, 0],
+            CommandPacket::Reset => [4, 0, 0, 0, 0, 0],
+            CommandPacket::Unknown => [0; COMMAND_BYTE_SEQUENCE_LENGTH],
+        }
+    }
+}
+
 impl From<(u16, u16, u16)> for CommandPacket {
     fn from((header, data_1, data_2): (u16, u16, u16)) -> Self {
         let command_type_byte = header & 0xff;
         match command_type_byte {
-            0 => CommandPacket::Beep((data_1 & 0xff) as u8),
-            1 => CommandPacket::SetAlarm(Time {
+            1 => CommandPacket::Beep((data_1 & 0xff) as u8),
+            2 => CommandPacket::SetAlarm(Time {
                 hours: (data_1 & 0xff) as u8,
                 minutes: ((data_1 & 0xff00) >> 8) as u8,
                 seconds: (data_2 & 0xff) as u8,
             }),
-            2 => CommandPacket::GetAlarm,
-            3 => CommandPacket::Reset,
+            3 => CommandPacket::GetAlarm,
+            4 => CommandPacket::Reset,
             _ => CommandPacket::Unknown,
         }
     }
@@ -31,15 +46,26 @@ mod tests {
     use super::*;
 
     #[test]
+    fn default_command_bytes() {
+        assert_eq!(
+            CommandByteSequence::default(),
+            [0; COMMAND_BYTE_SEQUENCE_LENGTH]
+        );
+    }
+
+    #[test]
     fn beep_command() {
-        assert_eq!(CommandPacket::from((0, 1, 0)), CommandPacket::Beep(1));
-        assert_eq!(CommandPacket::from((0, 15, 0)), CommandPacket::Beep(15));
+        assert_eq!(CommandPacket::from((1, 1, 0)), CommandPacket::Beep(1));
+        assert_eq!(CommandPacket::from((1, 15, 0)), CommandPacket::Beep(15));
+
+        assert_eq!(CommandPacket::Beep(1).to_bytes(), [1, 0, 1, 0, 0, 0]);
+        assert_eq!(CommandPacket::Beep(15).to_bytes(), [1, 0, 15, 0, 0, 0]);
     }
 
     #[test]
     fn set_alarm_command() {
         assert_eq!(
-            CommandPacket::from((1, 0x2112, 17)),
+            CommandPacket::from((2, 0x2112, 17)),
             CommandPacket::SetAlarm(Time {
                 hours: 18,
                 minutes: 33,
@@ -48,29 +74,58 @@ mod tests {
         );
 
         assert_eq!(
-            CommandPacket::from((1, 0x1221, 1)),
+            CommandPacket::from((2, 0x1221, 1)),
             CommandPacket::SetAlarm(Time {
                 hours: 33,
                 minutes: 18,
                 seconds: 1,
             })
         );
+
+        assert_eq!(
+            CommandPacket::SetAlarm(Time {
+                hours: 18,
+                minutes: 33,
+                seconds: 17,
+            })
+            .to_bytes(),
+            [2, 0, 18, 33, 17, 0]
+        );
+        assert_eq!(
+            CommandPacket::SetAlarm(Time {
+                hours: 33,
+                minutes: 18,
+                seconds: 1,
+            })
+            .to_bytes(),
+            [2, 0, 33, 18, 1, 0]
+        );
     }
 
     #[test]
     fn get_alarm_command() {
-        assert_eq!(CommandPacket::from((2, 0, 0)), CommandPacket::GetAlarm);
-        assert_eq!(CommandPacket::from((2, 11, 22)), CommandPacket::GetAlarm);
+        assert_eq!(CommandPacket::from((3, 0, 0)), CommandPacket::GetAlarm);
+        assert_eq!(CommandPacket::from((3, 11, 22)), CommandPacket::GetAlarm);
+
+        assert_eq!(CommandPacket::GetAlarm.to_bytes(), [3, 0, 0, 0, 0, 0]);
     }
 
     #[test]
     fn reset_command() {
-        assert_eq!(CommandPacket::from((3, 0, 0)), CommandPacket::Reset);
+        assert_eq!(CommandPacket::from((4, 0, 0)), CommandPacket::Reset);
+
+        assert_eq!(CommandPacket::Reset.to_bytes(), [4, 0, 0, 0, 0, 0]);
     }
 
     #[test]
     fn unknown_command() {
-        assert_eq!(CommandPacket::from((4, 0, 0)), CommandPacket::Unknown);
+        assert_eq!(CommandPacket::from((0, 0, 0)), CommandPacket::Unknown);
+        assert_eq!(CommandPacket::from((5, 0, 0)), CommandPacket::Unknown);
         assert_eq!(CommandPacket::from((10, 11, 22)), CommandPacket::Unknown);
+
+        assert_eq!(
+            CommandPacket::Unknown.to_bytes(),
+            [0; COMMAND_BYTE_SEQUENCE_LENGTH]
+        );
     }
 }
