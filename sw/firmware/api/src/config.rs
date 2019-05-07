bcef263b2fb41d1f1cb74792503ec455a50aad44
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2019-05-07 21:45:16 +0200

    Use dedicated and unique VID and PID: http://pid.codes/1209/DEED/.

--- sw/firmware/api/src/config.rs
+++ sw/firmware/api/src/config.rs
@@ -1,6 +1,6 @@
 pub const CLOCK_SPEED: u32 = 8_000_000;
-pub const DEVICE_VID: u16 = 0xffff;
-pub const DEVICE_PID: u16 = 0xffff;
+pub const DEVICE_VID: u16 = 0x0912;
+pub const DEVICE_PID: u16 = 0xedde;
 
 #[cfg(test)]
 mod tests {
