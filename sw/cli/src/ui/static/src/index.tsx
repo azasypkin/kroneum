75163eabcaade071ee39b3583beb4b519adfa101
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2019-12-26 18:16:56 +0100

    [cli] properly handle loading state of the Web UI.

--- sw/cli/src/ui/static/src/index.tsx
+++ sw/cli/src/ui/static/src/index.tsx
@@ -19,6 +19,7 @@ import {
   EuiFlexItem,
   EuiPopover,
   EuiComboBox,
+  EuiLoadingContent,
 } from '@elastic/eui';
 import axios from 'axios';
 
@@ -33,16 +34,174 @@ interface DeviceInfo {
   flash: number[];
 }
 
+const DeviceIdentifierSection = ({ identifier }: { identifier?: DeviceInfo['identifier'] }) => {
+  const content = identifier ? (
+    <EuiPanel style={{ maxWidth: 300 }}>
+      <EuiFormRow label="Bus" display="columnCompressed" style={{ alignItems: 'center' }}>
+        <EuiText size="s">{identifier?.bus ?? 'Unknown'}</EuiText>
+      </EuiFormRow>
+      <EuiFormRow label="Address" display="columnCompressed" style={{ alignItems: 'center' }}>
+        <EuiText size="s">{identifier?.address ?? 'Unknown'}</EuiText>
+      </EuiFormRow>
+      <EuiFormRow label="Vendor ID" display="columnCompressed" style={{ alignItems: 'center' }}>
+        <EuiText size="s">{identifier ? `0x${identifier.vendorID.toString(16).toUpperCase()}` : 'Unknown'}</EuiText>
+      </EuiFormRow>
+      <EuiFormRow label="Product ID" display="columnCompressed" style={{ alignItems: 'center' }}>
+        <EuiText size="s">{identifier ? `0x${identifier.productID.toString(16).toUpperCase()}` : 'Unknown'}</EuiText>
+      </EuiFormRow>
+      <EuiFormRow label="Manufacturer" display="columnCompressed" style={{ alignItems: 'center' }}>
+        <EuiText size="s">{identifier?.manufacturer ?? 'Unknown'}</EuiText>
+      </EuiFormRow>
+    </EuiPanel>
+  ) : (
+    <EuiPanel style={{ maxWidth: 300 }}>
+      <EuiLoadingContent lines={5} />
+    </EuiPanel>
+  );
+
+  return (
+    <EuiFlexItem>
+      <EuiPageContentHeader>
+        <EuiPageContentHeaderSection>
+          <EuiTitle>
+            <h2>Device information</h2>
+          </EuiTitle>
+        </EuiPageContentHeaderSection>
+      </EuiPageContentHeader>
+      <EuiPageContentBody>{content}</EuiPageContentBody>
+    </EuiFlexItem>
+  );
+};
+
+const DeviceFlashContentSection = ({ slots }: { slots?: number[] }) => {
+  const content = slots ? (
+    <EuiPanel style={{ maxWidth: 300 }}>
+      {slots.map((slotContent, slotIndex) => {
+        return (
+          <EuiFormRow
+            key={slotIndex}
+            label={`Slot#${slotIndex + 1}`}
+            display="columnCompressed"
+            style={{ alignItems: 'center' }}
+          >
+            <EuiText size="s">{slotContent}</EuiText>
+          </EuiFormRow>
+        );
+      })}
+    </EuiPanel>
+  ) : (
+    <EuiPanel style={{ maxWidth: 300 }}>
+      <EuiLoadingContent lines={5} />
+    </EuiPanel>
+  );
+  return (
+    <EuiFlexItem>
+      <EuiPageContentHeader>
+        <EuiPageContentHeaderSection>
+          <EuiTitle>
+            <h2>Flash slots</h2>
+          </EuiTitle>
+        </EuiPageContentHeaderSection>
+      </EuiPageContentHeader>
+      <EuiPageContentBody>{content}</EuiPageContentBody>
+    </EuiFlexItem>
+  );
+};
+
+const DeviceAlarmSection = () => {
+  return (
+    <EuiFlexItem>
+      <EuiPageContentHeader>
+        <EuiPageContentHeaderSection>
+          <EuiTitle>
+            <h2>Current alarm</h2>
+          </EuiTitle>
+        </EuiPageContentHeaderSection>
+      </EuiPageContentHeader>
+      <EuiPageContentBody>
+        <EuiPanel style={{ maxWidth: 300 }}>
+          <EuiFormRow label="Hours" display="columnCompressed" style={{ alignItems: 'center' }}>
+            <EuiText size="s">{'Unknown'}</EuiText>
+          </EuiFormRow>
+          <EuiFormRow label="Minutes" display="columnCompressed" style={{ alignItems: 'center' }}>
+            <EuiText size="s">{'Unknown'}</EuiText>
+          </EuiFormRow>
+          <EuiFormRow label="Seconds" display="columnCompressed" style={{ alignItems: 'center' }}>
+            <EuiText size="s">{'Unknown'}</EuiText>
+          </EuiFormRow>
+        </EuiPanel>
+      </EuiPageContentBody>
+    </EuiFlexItem>
+  );
+};
+
+const DeviceDiagnosticsSection = () => {
+  const [echoByteSequence, setEchoByteSequence] = useState<number[]>([0]);
+  const [isEchoPopOverOpen, setIsEchoPopOverOpen] = useState<boolean>(false);
+
+  const echoButton = <EuiButton onClick={() => setIsEchoPopOverOpen(true)}>Send echo</EuiButton>;
+
+  return (
+    <EuiFlexItem>
+      <EuiPageContentHeader>
+        <EuiPageContentHeaderSection>
+          <EuiTitle>
+            <h2>Diagnostics</h2>
+          </EuiTitle>
+        </EuiPageContentHeaderSection>
+      </EuiPageContentHeader>
+      <EuiPageContentBody>
+        <EuiPanel style={{ maxWidth: 300 }}>
+          <EuiFormRow style={{ alignItems: 'center' }} display="columnCompressed">
+            <EuiButton onClick={() => axios.get('/api/beep')}>Send beep</EuiButton>
+          </EuiFormRow>
+          <EuiFormRow style={{ alignItems: 'center' }} display="columnCompressed">
+            <EuiButton onClick={() => axios.get('/api/melody')}>Play melody</EuiButton>
+          </EuiFormRow>
+          <EuiFormRow style={{ alignItems: 'center' }} display="columnCompressed">
+            <EuiPopover
+              id="trapFocus"
+              ownFocus
+              button={echoButton}
+              isOpen={isEchoPopOverOpen}
+              closePopover={() => {
+                setIsEchoPopOverOpen(false);
+                setEchoByteSequence([0]);
+              }}
+            >
+              <EuiComboBox
+                style={{ minWidth: 300 }}
+                placeholder="Choose echo content"
+                options={Array.from({ length: 256 }).map((_, index) => ({ label: index.toString() }))}
+                selectedOptions={echoByteSequence.map(value => ({ label: value.toString() }))}
+                onChange={selectedOptions => setEchoByteSequence(selectedOptions.map(({ label }) => parseInt(label)))}
+                isClearable={true}
+              />
+
+              <EuiSpacer />
+
+              <EuiButton
+                isDisabled={echoByteSequence.length === 0}
+                fill
+                onClick={() =>
+                  axios.post('/api/echo', echoByteSequence).then(({ data }) => console.log(`Echo result: ${data}`))
+                }
+              >
+                Send
+              </EuiButton>
+            </EuiPopover>
+          </EuiFormRow>
+        </EuiPanel>
+      </EuiPageContentBody>
+    </EuiFlexItem>
+  );
+};
+
 const IndexPage = () => {
   const [info, setInfo] = useState<DeviceInfo | null>(null);
   useEffect(() => {
     axios.get('/api/info').then(({ data }) => setInfo(data));
   }, []);
-
-  const [echo, setEcho] = useState<number[]>([0]);
-  const [isEchoPopOverOpen, setIsEchoPopOverOpen] = useState<boolean>(false);
-
-  const echoButton = <EuiButton onClick={() => setIsEchoPopOverOpen(true)}>Send Echo</EuiButton>;
 
   return (
     <EuiPage>
@@ -56,128 +215,11 @@ const IndexPage = () => {
         </EuiPageHeader>
         <EuiPageContent>
           <EuiFlexGroup>
-            <EuiFlexItem>
-              <EuiPageContentHeader>
-                <EuiPageContentHeaderSection>
-                  <EuiTitle>
-                    <h2>Device information</h2>
-                  </EuiTitle>
-                </EuiPageContentHeaderSection>
-              </EuiPageContentHeader>
-              <EuiPageContentBody>
-                <EuiPanel style={{ maxWidth: 300 }}>
-                  <EuiFormRow label="Bus" display="columnCompressed" style={{ alignItems: 'center' }}>
-                    <EuiText size="s">{info?.identifier.bus ?? 'Unknown'}</EuiText>
-                  </EuiFormRow>
-                  <EuiFormRow label="Address" display="columnCompressed" style={{ alignItems: 'center' }}>
-                    <EuiText size="s">{info?.identifier.address ?? 'Unknown'}</EuiText>
-                  </EuiFormRow>
-                  <EuiFormRow label="Vendor ID" display="columnCompressed" style={{ alignItems: 'center' }}>
-                    <EuiText size="s">
-                      {info ? `0x${info.identifier.vendorID.toString(16).toUpperCase()}` : 'Unknown'}
-                    </EuiText>
-                  </EuiFormRow>
-                  <EuiFormRow label="Product ID" display="columnCompressed" style={{ alignItems: 'center' }}>
-                    <EuiText size="s">
-                      {info ? `0x${info.identifier.productID.toString(16).toUpperCase()}` : 'Unknown'}
-                    </EuiText>
-                  </EuiFormRow>
-                  <EuiFormRow label="Manufacturer" display="columnCompressed" style={{ alignItems: 'center' }}>
-                    <EuiText size="s">{info?.identifier.manufacturer ?? 'Unknown'}</EuiText>
-                  </EuiFormRow>
-                </EuiPanel>
-              </EuiPageContentBody>
-            </EuiFlexItem>
-            <EuiFlexItem>
-              <EuiPageContentHeader>
-                <EuiPageContentHeaderSection>
-                  <EuiTitle>
-                    <h2>Current alarm</h2>
-                  </EuiTitle>
-                </EuiPageContentHeaderSection>
-              </EuiPageContentHeader>
-              <EuiPageContentBody>
-                <EuiPanel style={{ maxWidth: 300 }}>
-                  <EuiFormRow label="Seconds" display="columnCompressed" style={{ alignItems: 'center' }}>
-                    <EuiText size="s">{info?.identifier.bus ?? 'Unknown'}</EuiText>
-                  </EuiFormRow>
-                </EuiPanel>
-              </EuiPageContentBody>
-            </EuiFlexItem>
-            <EuiFlexItem>
-              <EuiPageContentHeader>
-                <EuiPageContentHeaderSection>
-                  <EuiTitle>
-                    <h2>Flash slots</h2>
-                  </EuiTitle>
-                </EuiPageContentHeaderSection>
-              </EuiPageContentHeader>
-              <EuiPageContentBody>
-                <EuiPanel style={{ maxWidth: 300 }}>
-                  {(info?.flash ?? []).map((slotContent, slotIndex) => {
-                    return (
-                      <EuiFormRow
-                        key={slotIndex}
-                        label={`Slot#${slotIndex + 1}`}
-                        display="columnCompressed"
-                        style={{ alignItems: 'center' }}
-                      >
-                        <EuiText size="s">{slotContent}</EuiText>
-                      </EuiFormRow>
-                    );
-                  })}
-                </EuiPanel>
-              </EuiPageContentBody>
-            </EuiFlexItem>
+            <DeviceIdentifierSection identifier={info?.identifier} />
+            <DeviceFlashContentSection slots={info?.flash} />
+            <DeviceAlarmSection />
+            <DeviceDiagnosticsSection />
           </EuiFlexGroup>
-          <EuiSpacer size="xl" />
-          <EuiPageContentHeader>
-            <EuiPageContentHeaderSection>
-              <EuiTitle>
-                <h2>Device actions</h2>
-              </EuiTitle>
-            </EuiPageContentHeaderSection>
-          </EuiPageContentHeader>
-          <EuiPageContentBody>
-            <EuiPanel style={{ maxWidth: 300 }}>
-              <EuiFormRow style={{ alignItems: 'center' }} display="columnCompressed">
-                <EuiButton onClick={() => axios.get('/api/beep')}>Beep</EuiButton>
-              </EuiFormRow>
-              <EuiFormRow style={{ alignItems: 'center' }} display="columnCompressed">
-                <EuiButton onClick={() => axios.get('/api/melody')}>Play melody</EuiButton>
-              </EuiFormRow>
-              <EuiSpacer />
-              <EuiPopover
-                id="trapFocus"
-                ownFocus
-                button={echoButton}
-                isOpen={isEchoPopOverOpen}
-                closePopover={() => {
-                  setIsEchoPopOverOpen(false);
-                  setEcho([0]);
-                }}
-              >
-                <EuiComboBox
-                  style={{ minWidth: 300 }}
-                  placeholder="Choose echo content"
-                  options={Array.from({ length: 256 }).map((_, index) => ({ label: index.toString() }))}
-                  selectedOptions={echo.map(value => ({ label: value.toString() }))}
-                  onChange={selectedOptions => setEcho(selectedOptions.map(({ label }) => parseInt(label)))}
-                  isClearable={true}
-                />
-
-                <EuiSpacer />
-
-                <EuiButton
-                  isDisabled={echo.length === 0}
-                  fill
-                  onClick={() => axios.post('/api/echo', echo).then(({ data }) => console.log(`Echo result: ${data}`))}
-                >
-                  Send
-                </EuiButton>
-              </EuiPopover>
-            </EuiPanel>
-          </EuiPageContentBody>
         </EuiPageContent>
       </EuiPageBody>
     </EuiPage>
